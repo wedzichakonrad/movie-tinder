40e83fbd79424e755c6c3bedf962f88a53de2a3a
konradwedzicha <progmaster2003@gmail.com>
2024-10-19 10:44:55 +0200

    added some jest dependencies and started working on swipe animation

--- src/components/movie-card/movie-card.tsx
+++ src/components/movie-card/movie-card.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as Cross } from '../../assets/cross.svg';
 import MovieCardButton, {MovieCardButtonVariants} from "./button/movie-card-button";
 import {MovieProps} from "../../utils/types";
 import {updateMovieRecommendation} from "../../api/update-movie-recommendation";
-import {useCurrentMovie} from "../../providers/current-movie-provider";
+import React, {useRef} from "react";
 
 type MovieCardProps = {
     movie: MovieProps
@@ -13,15 +13,22 @@ type MovieCardProps = {
 }
 
 const MovieCard = ({movie, onClick, disabled}:MovieCardProps) => {
-    const context = useCurrentMovie();
+    const ItemType = 'CARD';
+    const ref = useRef(null);
 
+    const onDragStart = (e: React.DragEvent) => {
+        console.log(e,)
+        // @ts-ignore
+        ref.current.style.opacity = '0';
+        e.dataTransfer.setData("sth", 'value')
+    }
     const onButtonClick = (variant: string) => {
         onClick?.();
         // updateMovieRecommendation({variant, id: movie.id, onPending: setIsPending})
     }
 
     return (
-        <div className='movie-card'>
+        <div ref={ref} onDragStart={onDragStart} className='movie-card' draggable>
             <div className='movie-card__inner'>
                 <div className='movie-card__heading-wrapper'>
                     <h2>{movie.title}</h2>
